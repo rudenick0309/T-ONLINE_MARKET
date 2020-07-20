6f115c6b6daab0c9ace5d5c7218b87196627b171
rudenick0309 <rudenick0309@gmail.com>
2020-07-21 01:00:47 +0900

    'add_page_of_Bucket'

---
+++ pages/Bucket.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Bucket = () => {
+  return (
+    <>
+      <Text>버킷</Text>
+      </>
+  )
+}
+
+export default Bucket;
