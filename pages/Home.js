b0329bedc7efbee0f5e217366f8c8236805781b2
rudenick0309 <rudenick0309@gmail.com>
2020-07-21 01:02:05 +0900

    'add_page_of_Home'

---
+++ pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Home = () => {
+  return(
+    <>
+      <Text> adsf홈</Text>
+      </>
+  )
+}
+
+export default Home;
