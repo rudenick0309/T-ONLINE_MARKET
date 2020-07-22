18eceefff69a29c5fc57c8de790f453d84cb1546
rudenick0309 <rudenick0309@gmail.com>
2020-07-22 17:56:56 +0900

    'add_RecommendFlower_component'

---
+++ components/RecommendFlower.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const RecommendedFlower = () => {
+  return (
+    <View>
+      <Text>RecommendedFlower component</Text>
+    </View>
+  )
+}
+
+export default  RecommendedFlower;
+
+
+/*
+* {
+  recommendation: [
+    {
+      id: "Int",
+      title: "String",
+      img: "String",
+      contents: "String",
+    },
+    {
+      id: "Int",
+      title: "String",
+      img: "String",
+      contents: "String",
+    },
+  ], // max = 4 ~ 5
+
+}
+* */
