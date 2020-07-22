c278d3d0ad2e0d5f1dc629d233d9ab8bb8991226
rudenick0309 <rudenick0309@gmail.com>
2020-07-22 18:01:29 +0900

    'add_page_of_Bucket'

--- pages/Bucket.js
+++ pages/Bucket.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from "react";
+import {StyleSheet, Text, View} from "react-native";
+import styled from "styled-components";
+import Header from "../components/Header";
+import Nav from '../components/Nav'
 
-const Bucket = () => {
+// css part
+const Container = styled.SafeAreaView`
+  flex: 1;
+  border : 2px solid blue
+`;
+
+const Contents = styled.ScrollView`
+  flex: 1;
+  border : 2px solid blue
+`;
+
+
+// function part
+const Bucket = (props) => {
+  // TODO : changes the state name :  const [checkList, setCheckList] = useState(null);
+
+  useEffect(() => {
+    // TODO: take the bucket list to axios
+  }, []);
+
   return (
-    <>
-      <Text>버킷</Text>
-      </>
-  )
-}
+    <Container>
+      <Header props={props}/>
+      <Contents><Text>장바구니 페이지</Text></Contents>
+      <Nav props={props}/>
+    </Container>
+  );
+};
 
 export default Bucket;
