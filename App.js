d5ad6d3d72b98c9a5650df96270f89a0c358e6bb
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-07-22 21:48:44 +0900

    ADD App'

---
+++ App.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import styled from 'styled-components'
+import 'react-native-gesture-handler';
+import AppNavigator from "./navigators/AppNavigator";
+
+//TODO : after test, delete or apply the below code ( HomeScreen, SettingScreen )
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// TODO: apply themes in NavigationContainer of App
+const Theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#ffffff',
+  },
+};
+
+// TODO: after test, delete or apply the below code ( HomeScreend, SettingScreen )
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Home!</Text>
+//     </View>
+//   );
+// }
+//
+// function SettingsScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Settings!</Text>
+//     </View>
+//   );
+// }
+// const Tab = createBottomTabNavigator();
+// function Apps() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator>
+//         <Tab.Screen name="Home" component={HomeScreen} />
+//         <Tab.Screen name="Settings" component={SettingsScreen} />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+//
+
+const App = () => {
+  return (
+    <>
+
+      <StatusBar style="auto" />
+
+      <NavigationContainer theme={Theme} >
+        <AppNavigator/>
+        {/*<Apps />*/}
+      </NavigationContainer>
+
+    </>
+  );
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
