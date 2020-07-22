98c0b7186bf4f91b5438b3e42392b0e2fe0a6a29
rudenick0309 <rudenick0309@gmail.com>
2020-07-22 17:53:29 +0900

    'add_BestFlower_component'

---
+++ components/BestFlower.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const BestFlower = () => {
+  return (
+    <View>
+      <Text>BestFlower component</Text>
+    </View>
+  )
+}
+
+export default BestFlower
+
+/*
+* {
+  best: [
+    {
+      id: "Int",
+      title: "String",
+      img: "String",
+      price: "String",
+    },
+    {
+      id: "Int",
+      title: "String",
+      img: "String",
+      price: "String",
+    },
+  ], // max = 10
+}
+* */
