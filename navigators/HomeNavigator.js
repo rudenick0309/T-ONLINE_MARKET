35bb5e4a2519509eebc77d3c9ff8cb4e05dda0b8
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-07-24 11:28:04 +0900

    FEAT homnavigator

--- navigators/HomeNavigator.js
+++ navigators/HomeNavigator.js
@@ -7,8 +7,13 @@ import BestFlower from "../components/BestFlower";
 //TODO: import Logout from '../pages/Logout'
 //TODO: import Mypage from '../pages/Myapge'
 //TODO: import Signup from '../pages/Signup'
-import TestPage from '../pages/TestPage'
-import TestPageNav from '../pages/TestPageNav'
+// import TestPage from '../pages/TestPage'
+// import TestPageNav from '../pages/TestPageNav'
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+import Mypage from "../pages/Mypage";
+import Myinfocheck from "../pages/Myinfocheck";
+import Myinfo from "../pages/Myinfo";
 
 const Stack = createStackNavigator();
 
@@ -19,12 +24,14 @@ const HomeNavigator = () => {
       {/*<Stack.Screen name="RecommendedFlower" component={RecommendedFlower}/>*/}
       {/*<Stack.Screen name="BestFlower" component={BestFlower}/>*/}
       <Stack.Screen name="BestFlower" component={BestFlower}/>
-      {/* TODO: <Stack.Screen name="Login" component={Login}/>*/}
+      <Stack.Screen name="SignIn" component={SignIn}/>
       {/* TODO: <Stack.Screen name="Logout" component={Logout}/>*/}
-      {/* TODO: <Stack.Screen name="Mypage" component={Mypage}/>*/}
-      {/* TODO: <Stack.Screen name="Signup" component={Signup}/>*/}
-       <Stack.Screen name="TestPage" component={TestPage}/>
-       <Stack.Screen name="TestPageNav" component={TestPageNav}/>
+       <Stack.Screen name="Mypage" component={Mypage}/>
+      <Stack.Screen name="SignUp" component={SignUp}/>
+      <Stack.Screen name="Myinfocheck" component={Myinfocheck}/>
+      <Stack.Screen name="Myinfo" component={Myinfo}/>
+       {/* <Stack.Screen name="TestPage" component={TestPage}/>
+       <Stack.Screen name="TestPageNav" component={TestPageNav}/> */}
 
     </Stack.Navigator>
   );
