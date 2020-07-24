14c1d4422e006c6d61cd3060f25b0c0475f724e6
rudenick0309 <rudenick0309@gmail.com>
2020-07-24 10:57:40 +0900

    'feat_route'

--- navigators/BucketNavigator.js
+++ navigators/BucketNavigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {createStackNavigator} from "@react-navigation/stack";
 import Bucket from "../pages/Bucket";
-import RecommendedFlower from "../components/RecommendFlower";
+import RecommendedFlower from "../components/RecommendedFlower";
 import BestFlower from "../components/BestFlower";
 import TestPage from "../pages/TestPage";
 import TestPageNav from "../pages/TestPageNav";
@@ -11,12 +11,11 @@ const Stack = createStackNavigator();
 
 const BucketNavigator = () => {
   return (
+
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Bucket" component={Bucket}/>
-      {/*<Stack.Screen name="Home" component={Home}/>*/}
-      <Stack.Screen name="TestPage" component={TestPage}/>
-      <Stack.Screen name="TestPageNav" component={TestPageNav}/>
     </Stack.Navigator>
+
   );
 };
 
