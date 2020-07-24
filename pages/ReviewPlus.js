d05cd7ea59be6d30c6bdd3af2c95ba7859c34ed8
rudenick0309 <rudenick0309@gmail.com>
2020-07-25 08:19:28 +0900

    'add_users_writes_R_about_specific_goods'

---
+++ pages/ReviewPlus.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from "react";
+import {StyleSheet, Text, View} from "react-native";
+import styled from "styled-components";
+import Header from "../components/Header";
+import Nav from '../components/Nav'
+
+// css part
+const Container = styled.SafeAreaView`
+  flex: 1;
+  border : 2px solid blue
+`;
+
+const Contents = styled.ScrollView`
+  flex: 1;
+  border : 2px solid blue
+`;
+
+
+// function part
+const ReviewPlus = (props) => {
+  // TODO : changes the state name :  const [checkList, setCheckList] = useState(null);
+
+  useEffect(() => {
+    // TODO: take the bucket list to axios
+  }, []);
+
+  return (
+    <Container>
+      <Header props={props}/>
+      <Contents><Text>ReviewPlus</Text></Contents>
+      <Nav props={props}/>
+    </Container>
+  );
+};
+
+export default ReviewPlus;
