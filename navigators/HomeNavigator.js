b713e8eb8bc091aa667d76d9f85158ce335a2675
rudenick0309 <rudenick0309@gmail.com>
2020-07-25 08:06:56 +0900

    'simply_modified'

--- navigators/HomeNavigator.js
+++ navigators/HomeNavigator.js
@@ -5,23 +5,28 @@ import GoodsDetail from "../pages/GoodsDetail";
 import GoodsList from '../pages/GoodsList'
 import Bucket from "../pages/Bucket";
 import Payment from "../pages/Payment";
+import QnAPlus from "../pages/QnAPlus";
+import ReviewPlus from "../pages/ReviewPlus";
+
 //TODO: import Login from '../pages/Login'
 //TODO: import Logout from '../pages/Logout'
 //TODO: import Mypage from '../pages/Myapge'
 //TODO: import Signup from '../pages/Signup'
-import TestPage from '../pages/TestPage'
-import TestPageNav from '../pages/TestPageNav'
+
 
 const Stack = createStackNavigator();
 
 const HomeNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
+
       <Stack.Screen name="Home" component={Home}/>
       <Stack.Screen name="GoodsDetail" component={GoodsDetail}/>
       <Stack.Screen name="GoodsList" component={GoodsList}/>
       <Stack.Screen name="Payment" component={Payment}/>
       <Stack.Screen name="Bucket" component={Bucket}/>
+      <Stack.Screen name="QnAPlus" component={QnAPlus}/>
+      <Stack.Screen name="ReviewPlus" component={ReviewPlus}/>
 
       {/* TODO: <Stack.Screen name="Login" component={Login}/>*/}
       {/* TODO: <Stack.Screen name="Logout" component={Logout}/>*/}
