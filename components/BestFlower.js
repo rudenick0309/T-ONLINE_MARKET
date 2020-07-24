b8916ed60a78b2534a23e31cfe5fb142524ee4b4
rudenick0309 <rudenick0309@gmail.com>
2020-07-24 10:53:34 +0900

    'style_css'

--- components/BestFlower.js
+++ components/BestFlower.js
@@ -1,30 +1,67 @@
-import React from 'react';
+import React from "react";
+import {StyleSheet, Text, View, Input, TouchableOpacity, Image} from "react-native";
+import styled from "styled-components";
 
-const BestFlower = () => {
-  return (
-    <View>
-      <Text>BestFlower component</Text>
-    </View>
-  )
-}
+// css part
+export const BestFlowerContainer = styled.TouchableOpacity`
+  flex: 1;
+  border: 3px solid blue
+`;
 
-export default BestFlower
+export const BestFlowerContents = styled.ScrollView`
+  flex: 1;
+  border: 3px solid grey;
+`;
+
+export const BestFlowerImageView = styled.Image`
+  flex:1
+  height: 160px;
+  border: 2px solid yellow;
+`;
+
+export const BestFlowerTextView = styled.Text`
+  color : red;
+`;
+//
+// // function part
+// const RecommendedFlower = ({data}) => {
+//   // const data = data;
+//   const {title, img, contents} = data;
+//   // const {onPress} = this.props
+//
+//   return (
+//     <Container >
+//       <Contents>
+//         <ImageView source={img}/>
+//         <TextView>{contents}</TextView>
+//         {/*<View>*/}
+//         {/*</View>*/}
+//         <TextView>{title}</TextView>
+//       </Contents>
+//       {/*<Contents>{data.title}</Contents>*/}
+//     </Container>
+//   );
+// };
+//
+// export default RecommendedFlower;
+
 
 /*
 * {
-  best: [
+  recommendation: [
     {
       id: "Int",
       title: "String",
       img: "String",
-      price: "String",
+      contents: "String",
     },
     {
       id: "Int",
       title: "String",
       img: "String",
-      price: "String",
+      contents: "String",
     },
-  ], // max = 10
+  ], // max = 4 ~ 5
+
 }
 * */
