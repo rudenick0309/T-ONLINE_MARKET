9a634083f45d636204dc1f2d73db32c1fbead5db
rudenick0309 <rudenick0309@gmail.com>
2020-07-25 13:31:41 +0900

    'core_all_the_files_to_ReactNativeCli'

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import styled from 'styled-components'
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 import 'react-native-gesture-handler';
-import AppNavigator from "./navigators/AppNavigator";
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
-//TODO : after test, delete or apply the below code ( HomeScreen, SettingScreen )
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import AppNavigator from './navigators/AppNavigator';
 
-// TODO: apply themes in NavigationContainer of App
+
 const Theme = {
   ...DefaultTheme,
   colors: {
@@ -18,58 +35,56 @@ const Theme = {
   },
 };
 
-// TODO: after test, delete or apply the below code ( HomeScreend, SettingScreen )
-// function HomeScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Home!</Text>
-//     </View>
-//   );
-// }
-//
-// function SettingsScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Settings!</Text>
-//     </View>
-//   );
-// }
-// const Tab = createBottomTabNavigator();
-// function Apps() {
-//   return (
-//     <NavigationContainer>
-//       <Tab.Navigator>
-//         <Tab.Screen name="Home" component={HomeScreen} />
-//         <Tab.Screen name="Settings" component={SettingsScreen} />
-//       </Tab.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-//
-
-const App = () => {
+const App: () => React$Node = () => {
   return (
     <>
-
       <StatusBar style="auto" />
 
-      <NavigationContainer initialRouteName="Home" theme={Theme} >
-        <AppNavigator/>
+      <NavigationContainer initialRouteName="Home" theme={Theme}>
+        <AppNavigator />
         {/*<Apps />*/}
       </NavigationContainer>
-
     </>
   );
-}
+};
 
-export default App
+// const styles = StyleSheet.create({
+//   scrollView: {
+//     backgroundColor: Colors.lighter,
+//   },
+//   engine: {
+//     position: 'absolute',
+//     right: 0,
+//   },
+//   body: {
+//     backgroundColor: Colors.white,
+//   },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//     color: Colors.black,
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//     color: Colors.dark,
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+//   footer: {
+//     color: Colors.dark,
+//     fontSize: 12,
+//     fontWeight: '600',
+//     padding: 4,
+//     paddingRight: 12,
+//     textAlign: 'right',
+//   },
+// });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
