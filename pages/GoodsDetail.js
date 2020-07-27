be1adac00541f321db850b404c974e57f1122d30
rudenick0309 <rudenick0309@gmail.com>
2020-07-27 19:21:21 +0900

    'feat_for_redux'

--- pages/GoodsDetail.js
+++ pages/GoodsDetail.js
@@ -1,87 +1,86 @@
-import React, {useState, useEffect} from "react";
-import {StyleSheet, Text, View, Button} from "react-native";
-import styled from "styled-components";
-import Header from "../components/Header";
-import Nav from "../components/Nav";
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import styled from 'styled-components';
+import Header from '../components/Header';
+import Nav from '../components/Nav';
 // import RecommendedFlower from "../components/RecommendedFlower";
-import Payment from "./Payment";
-import Bucket from "./Bucket";
+import Payment from './Payment';
+import Bucket from './Bucket';
 
 // css part
 const Container = styled.SafeAreaView`
   flex: 1;
-  border : 2px solid blue;
+  border: 2px solid blue;
 `;
 
 const Contents = styled.ScrollView`
   flex: 1;
-  border : 2px solid blue;
+  border: 2px solid blue;
 `;
 
 const DetailInfoOfUpper = styled.View`
   flex: 1;
-  border : 2px solid red;
-  flex-direction:row;
+  border: 2px solid red;
+  flex-direction: row;
 `;
 
 const LeftDetailInfoOfUpper = styled.View`
   flex: 1;
-  width:200px;
-  height:200px;
-  border : 2px solid skyblue;
+  width: 200px;
+  height: 200px;
+  border: 2px solid skyblue;
 `;
 
 const RightDetailInfoOfUpper = styled.View`
   flex: 1;
-  width:200px;
-  height:200px;
-  border : 2px solid green;
+  width: 200px;
+  height: 200px;
+  border: 2px solid green;
 `;
 
 const DetailInfoOfBottom = styled.View`
-  flex:1;
+  flex: 1;
   border: 2px solid pink;
 `;
 
 const InfoDetailInfoOfBottom = styled.ScrollView`
-  flex:1;
+  flex: 1;
   height: 500px;
-  border: 2px solid pink
+  border: 2px solid pink;
 `;
 
 const QnADetailInfoOfBottom = styled.ScrollView`
-  flex:1;
+  flex: 1;
   height: 500px;
-  border: 2px solid pink
+  border: 2px solid pink;
 `;
 
 const QnAButtonDetailInfoOfBottom = styled.Button`
   height: 50px;
-  border: 2px solid pink
+  border: 2px solid pink;
 `;
 const ReviewButtonDetailInfoOfBottom = styled.Button`
   height: 50px;
-  border: 2px solid pink
+  border: 2px solid pink;
 `;
 
 const ReviewDetailInfoOfBottom = styled.ScrollView`
-  flex:1;
+  flex: 1;
   height: 500px;
-  border: 2px solid pink
+  border: 2px solid pink;
 `;
 
 const ViewDetailInfoOfBottom = styled.View`
-  flex-direction:row;
+  flex-direction: row;
   width: 50px;
-  flex:1;
+  flex: 1;
 `;
 
 // TODO: position : fixed? -> always stay in right and bottom corner in a mobile view?
 const ButtonDetailInfoOfBottom = styled.Button`
-  width:200px;
+  width: 200px;
   border: 3px solid black;
-`
-
+`;
 
 // function part
 const GoodsDetail = (props) => {
@@ -99,85 +98,94 @@ const GoodsDetail = (props) => {
 
   return (
     <Container>
-
-      <Header props={props}/>
+      <Header props={props} />
 
       <Contents>
-
         <DetailInfoOfUpper>
-          <LeftDetailInfoOfUpper></LeftDetailInfoOfUpper>
-          <RightDetailInfoOfUpper></RightDetailInfoOfUpper>
+          <LeftDetailInfoOfUpper />
+          <RightDetailInfoOfUpper />
         </DetailInfoOfUpper>
 
         {/* Distinct Line */}
         <DetailInfoOfBottom>
-
           <ViewDetailInfoOfBottom>
-            <ButtonDetailInfoOfBottom title={"info"} onPress={() => {
-              setQna(false);
-              setInfo(true);
-              setReview(false);
-            }}/>
-            <ButtonDetailInfoOfBottom title={"qna"} onPress={() => {
-              setQna(true);
-              setInfo(false);
-              setReview(false);
-            }}/>
-            <ButtonDetailInfoOfBottom title={"review"} onPress={() => {
-              setQna(false);
-              setInfo(false);
-              setReview(true);
-            }}/>
+            <ButtonDetailInfoOfBottom
+              title={'info'}
+              onPress={() => {
+                setQna(false);
+                setInfo(true);
+                setReview(false);
+              }}
+            />
+            <ButtonDetailInfoOfBottom
+              title={'qna'}
+              onPress={() => {
+                setQna(true);
+                setInfo(false);
+                setReview(false);
+              }}
+            />
+            <ButtonDetailInfoOfBottom
+              title={'review'}
+              onPress={() => {
+                setQna(false);
+                setInfo(false);
+                setReview(true);
+              }}
+            />
           </ViewDetailInfoOfBottom>
 
-          {info
-            ?
-            (
-              <InfoDetailInfoOfBottom>
-                <Text>info part</Text>
-              </InfoDetailInfoOfBottom>
-            )
-            : qna
-              ?
-              (
-                <View>
-                  <QnADetailInfoOfBottom>
-                    <Text>qna part</Text>
-                  {/*  TODO: Should this function(write a text) is a page? or component?*/}
-                  </QnADetailInfoOfBottom>
-                    <QnAButtonDetailInfoOfBottom title={'+'} onPress={()=> {props.navigation.navigate("QnAPlus" )}}/>
-
-                </View>
-              )
-              : review
-                ?
-                (
-                  <ReviewDetailInfoOfBottom>
-                    <Text>review part</Text>
-                    <Button title={'+'} onPress={()=> {props.navigation.navigate("ReviewPlus" )}}></Button>
-                    {/*  TODO: Should this function(write a text) is a page? or component?*/}
-                  </ReviewDetailInfoOfBottom>
-                )
-                : <Text>empty part</Text>
-          }
-
+          {info ? (
+            <InfoDetailInfoOfBottom>
+              <Text>info part</Text>
+            </InfoDetailInfoOfBottom>
+          ) : qna ? (
+            <View>
+              <QnADetailInfoOfBottom>
+                <Text>qna part</Text>
+                {/*  TODO: Should this function(write a text) is a page? or component?*/}
+                <QnAButtonDetailInfoOfBottom
+                  title={'+'}
+                  onPress={() => {
+                    props.navigation.navigate('QnAPlus');
+                  }}
+                />
+              </QnADetailInfoOfBottom>
+            </View>
+          ) : review ? (
+            <ReviewDetailInfoOfBottom>
+              <Text>review part</Text>
+              <ReviewButtonDetailInfoOfBottom
+                title={'+'}
+                onPress={() => {
+                  props.navigation.navigate('ReviewPlus');
+                }}
+              />
+              {/*  TODO: Should this function(write a text) is a page? or component?*/}
+            </ReviewDetailInfoOfBottom>
+          ) : (
+            <Text>empty part</Text>
+          )}
         </DetailInfoOfBottom>
-
 
         {/* Distinct Line */}
         {/*TODO: <Button title={'구매하기'} onPress={() => {props.navigation.navigate("Payment", {id : props.route.params.id} )}}/>*/}
         {/*TODO: <Button title={'장바구니 담기'} onPress={() => {props.navigation.navigate("Bucket", {id : props.route.params.id} )}}/>*/}
-        <Button title={"구매하기"} onPress={() => {
-          return props.navigation.navigate("Payment");
-        }}/>
-        <Button title={"장바구니 담기"} onPress={() => {
-          props.navigation.navigate("Bucket");
-        }}/>
-
+        <Button
+          title={'구매하기'}
+          onPress={() => {
+            return props.navigation.navigate('Payment');
+          }}
+        />
+        <Button
+          title={'장바구니 담기'}
+          onPress={() => {
+            props.navigation.navigate('Bucket');
+          }}
+        />
       </Contents>
 
-      <Nav props={props}/>
-
+      <Nav props={props} />
     </Container>
   );
 };
