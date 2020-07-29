30aaeee1a27b75f00d64ede7375f0221e53f5456
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-07-29 14:22:00 +0900

    FIX myinfi

--- pages/Myinfo.js
+++ pages/Myinfo.js
@@ -20,21 +20,28 @@ const Contents = styled.ScrollView`
 // function part
 const Myinfo = (props) => {
   // TODO : changes the state name :  const [checkList, setCheckList] = useState(null);
+ 
+  const [userInfo, setuserInfo] = useState([])
 
   useEffect(() => {
-    // TODO: take the bucket list to axios
+    axios.get("http://ec2-15-164-219-204.ap-northeast-2.compute.amazonaws.com:4000/user/info")
+      .then((result) => {
+        setuserInfo(result.data)
+        console.log(result.data, 'data')
+      });
   }, []);
 
   return (
     <Container>
       <Header props={props}/>
       <Contents>
-          <Text> Email </Text>
-          <Text> Name </Text>
-          <Text> Phone </Text>
-          <Text> Address </Text>
-          <Button title="비밀번호 변경" />
+          <Text> Email : {userInfo? userInfo.email: ''} </Text>
+          <Text> Name : {userInfo? userInfo.name: ''}</Text>
+          <Text> Phone : {userInfo? userInfo.phone: ''}</Text>
+          <Text> Address : {userInfo? userInfo.address: ''}</Text>
+          <Button title="비밀번호 변경" onPress={ () => {props.navigation.navigate('PasswordChange')}}/>
       <Button title ="수정하기" onPress={ () => {props.navigation.navigate('Myinfocheck')} } />
+      <Button title ="탈퇴하기" onPress={ () => {props.navigation.navigate('Resign')} } />
       </Contents>
       <Nav props={props}/>
     </Container>
