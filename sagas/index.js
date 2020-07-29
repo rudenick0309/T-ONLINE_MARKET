3325c13be32ef09255fa7e4dc1c309e5320d721b
rudenick0309 <rudenick0309@gmail.com>
2020-07-29 22:26:10 +0900

    "simply plus aws URL"

--- sagas/index.js
+++ sagas/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import goodsSaga from './goods';
 // TODO :  import userSaga from './user'   -> later
 
-// axios.defaults.baseURL = 'http://localhost:4000';  //TODO: backend port check
+axios.defaults.baseURL = 'http://ec2-15-164-219-204.ap-northeast-2.compute.amazonaws.com:4000';  //TODO: backend port check
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
