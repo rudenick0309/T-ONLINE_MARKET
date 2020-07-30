5e0a4c368fb46a1b2e1b2169db922eab4c7c9d7f
rudenick0309 <rudenick0309@gmail.com>
2020-07-31 03:25:14 +0900

    "add edits a QnA content"

---
+++ pages/QnAPlusEdit.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {View, Text} from "react-native";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import styled from "styled-components";
+
+
+// css part
+const Container = styled.ScrollView`
+  flex: 1;
+  border: 3px solid blue
+`;
+
+const InContainer = styled.TouchableOpacity`
+  border: 3px solid ivory
+`;
+
+const Contents = styled.View`
+  flex: 1;
+  border: 3px solid grey;
+`;
+
+const ImageView = styled.Image`
+  flex:1
+  border: 2px solid yellow;
+`;
+
+const TextView = styled.Text`
+  color : red;
+`;
+
+const QnAPlusEdit = (props) => {
+  console.log('In QnAPlusEdit props : ', props)   //TODO : must check console.log for study
+  return (
+    <Container>
+      <Header props={props}/>
+
+      <Contents>
+
+          <Text>QnAPlusEdit 페이지</Text>
+      </Contents>
+
+
+      <Nav props={props}/>
+    </Container>
+  );
+};
+
+export default QnAPlusEdit;
