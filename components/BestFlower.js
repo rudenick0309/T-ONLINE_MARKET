c76d162ba9d895f6940713309f9ea1bc1eeca78b
rudenick0309 <rudenick0309@gmail.com>
2020-08-02 20:55:35 +0900

    simply css layout - border line deleted

--- components/BestFlower.js
+++ components/BestFlower.js
@@ -12,19 +12,19 @@ import styled from 'styled-components';
 // css part
 export const BestFlowerContainer = styled.TouchableOpacity`
   flex: 1;
-  border: 3px solid blue
+  
 `;
 
 export const BestFlowerContents = styled.ScrollView`
   flex: 1;
-  border: 3px solid grey;
+  
 `;
 
 export const BestFlowerImageView = styled.Image`
   flex:1
   height: 160px;
   resize-mode: contain;
-  border: 2px solid yellow;
+  
 `;
 
 export const BestFlowerTextView = styled.Text`
