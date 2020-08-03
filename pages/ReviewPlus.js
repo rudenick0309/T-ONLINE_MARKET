03a670f4188de3d07fd7186041879d8339657035
rudenick0309 <rudenick0309@gmail.com>
2020-08-03 18:16:18 +0900

    "feat css layout and review CRUD function"

--- pages/ReviewPlus.js
+++ pages/ReviewPlus.js
@@ -1,33 +1,112 @@
-import React, {useState, useEffect} from "react";
-import {StyleSheet, Text, View} from "react-native";
+import React, {useState, useEffect, useCallback} from "react";
+import {StyleSheet, Text, View, TextInput} from "react-native";
 import styled from "styled-components";
 import Header from "../components/Header";
-import Nav from '../components/Nav'
+import Nav from "../components/Nav";
+import {connect, useDispatch, useSelector} from "react-redux";
+// import {actionCreators} from "../reducers/goods";
+import shortid from "shortid";
+import {addToReview} from "../reducers/goods";
+// import {initialState} from '../reducers/goods'
 
 // css part
 const Container = styled.SafeAreaView`
   flex: 1;
-  border : 2px solid blue
+  border: 2px solid blue;
 `;
 
 const Contents = styled.ScrollView`
   flex: 1;
-  border : 2px solid blue
+  border: 2px solid blue;
+`;
+
+// flex-direction:row;
+const ReviewView = styled.View`
+  border: 3px solid red;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const ReviewNameText = styled.TextInput`
+  width: 100px;
+  border: 2px solid yellow;
+`;
+
+const ReviewTitleText = styled.TextInput`
+  width: 100px;
+  border: 2px solid green;
+`;
+
+const ReviewButton = styled.Button`
+  width: 100px;
+  border: 2px solid yellow;
+`;
+
+const ReviewContentTextInput = styled.TextInput`
+  flex: 1;
+  border: 2px solid green;
+  height: 200px;
 `;
 
 
 // function part
 const ReviewPlus = (props) => {
-  // TODO : changes the state name :  const [checkList, setCheckList] = useState(null);
+  console.log('In ReviewPlus, props : ', props.route.params);
+  const [userName, onChangeUserName] = useState("");
+  const [title, onChangeTitle] = useState("");
+  const [content, onChangeContent] = useState("");
+  const dispatch = useDispatch();
+  const id = props.route.params;
+  const review = useSelector((state) => state.goods?.review);
+
+  console.log('In ReviewPlus, id : ', id);
 
   useEffect(() => {
     // TODO: take the bucket list to axios
   }, []);
 
+  const text = {
+    title: title,
+    contents: content,
+    goods_id:id.id,
+  };
+
+  const onPressReview = useCallback(() => {
+    console.log('In Review, text: ', text);
+    dispatch(addToReview(text));  //TODO : 1. text  or  2. (name, content)
+    props.navigation.goBack();
+  }, [userName, content]);
+
+
   return (
     <Container>
+
       <Header props={props}/>
-      <Contents><Text>ReviewPlus</Text></Contents>
+
+      <Contents>
+
+        <Text>
+          {
+            `리뷰를 등록해 주세요. 사랑합니다 갱님`
+          }
+        </Text>
+
+        <ReviewView>
+          <ReviewNameText maxLength={5} value={userName} onChangeText={(text) => onChangeUserName(text)}/>
+          <ReviewTitleText maxLength={20} value={title} onChangeText={(text) => onChangeTitle(text)}/>
+
+          <ReviewButton title={"등록하기"} onPress={onPressReview}/>
+
+        </ReviewView>
+
+        <ReviewContentTextInput
+          maxLength={200}
+          multiline={true}
+          onChangeText={(text) => onChangeContent(text)}
+          value={content}
+        />
+      </Contents>
+
       <Nav props={props}/>
     </Container>
   );
