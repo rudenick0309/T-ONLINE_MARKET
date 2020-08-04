6726d24165c5645b29a91fe8c26850204f3b6d3b
rudenick0309 <rudenick0309@gmail.com>
2020-08-04 15:21:38 +0900

    "simply button menu"

--- components/Header.js
+++ components/Header.js
@@ -1,44 +1,64 @@
 import React, {useState} from "react";
 import styled from "styled-components";
 import {View, Text, TouchableOpacity, TextInput} from "react-native";
+import Search from "../pages/Search";
 
+
+// border : 4px solid #05dfd7;
+// background-color: #f5efef;
 const HeaderView = styled.View`
   height: 50px;
   flex-direction: row;
   justify-content: space-around;
-  margin-top: 20px;
-`
-const HeaderIcon = styled.TouchableOpacity`
-  width : 50px;
-  height : 50px;
-  border : 3px solid blue;
-`
+  margin-top: 0px;
+  align-items: center;
+  background-color: #f5efef;
+`;
 
-const SearchIcon = styled.TextInput`
-  width : 150px;
+const HeaderIcon = styled.TouchableOpacity`
+  justify-content:center;
+  align-items:center;
+  width : 55px;
   height : 50px;
-  border : 3px solid blue;
-`
+`;
+
+const HeaderIconText = styled.Text`
+  color: #464e46;
+  font-weight : bold;
+`;
+
+const HeaderCenterText = styled.Text`
+  color: #464e46;
+  font-weight : bold;
+  font-size: 23px;
+`;
 
 const Header = ({props}) => {
   const [text, onChangeText] = useState("Here is Search part");
 
-
-
   return (
     <HeaderView>
-      <HeaderIcon onPress={ () => {props.navigation.navigate('Home')} } ><Text>로고</Text></HeaderIcon>
+      <HeaderIcon onPress={() => {
+        props.navigation.navigate("Home");
+      }}>
+        <HeaderIconText>HOME</HeaderIconText>
+      </HeaderIcon>
 
       {/* Find like the onClick event*/}
-      <SearchIcon
-        onChangeText={text => onChangeText(text)}
-        value={text}
-      />
+      {/*<SearchIcon*/}
+      {/*  onChangeText={text => onChangeText(text)}*/}
+      {/*  value={text}*/}
+      {/*/>*/}
 
+      <HeaderCenterText>T ONLINE MARKET</HeaderCenterText>
 
-      <HeaderIcon onPress={ () => {props.navigation.navigate('SignIn')} }><Text>로그인</Text></HeaderIcon>
+      <HeaderIcon onPress={() => {
+        props.navigation.navigate("Search");
+      }}>
+        <HeaderIconText>SEARCH</HeaderIconText>
+      </HeaderIcon>
     </HeaderView>
-  )
-}
+  );
+};
 
 export default Header;
