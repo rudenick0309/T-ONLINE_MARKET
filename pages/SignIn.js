86fa4956c57f0d9f13f607f859a9b0bfd9258292
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-08-04 11:51:54 +0900

    FIX signin

--- pages/SignIn.js
+++ pages/SignIn.js
@@ -1,25 +1,33 @@
-import React, { useState , useCallback} from 'react';
-import styled from "styled-components";
-import Header from "../components/Header";
-import Nav from '../components/Nav'
-import { StyleSheet,Button, Text, View, ScrollView, TextInput, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, {useState, useCallback} from 'react';
+import styled from 'styled-components';
+import Header from '../components/Header';
+import Nav from '../components/Nav';
+import {
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 // import { Actions } from 'react-native-router-flux';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {loginAction} from '../reducers/login';
 
 const Container = styled.SafeAreaView`
   flex: 1;
-  border : 2px solid blue
+  border: 2px solid blue;
 `;
 
 const Contents = styled.ScrollView`
   flex: 1;
-  border : 2px solid blue
+  border: 2px solid blue;
 `;
- 
+
 // const InputText = styled.TextInput`
 //         height: 40,
 // `
@@ -30,81 +38,59 @@ const InButton = styled.Button`
 `;
 
 const SignIn = (props) => {
-      
-      const dispatch = useDispatch();
-      const [email, setEmail] = useState('')
-      const [password, setPassword] = useState('')
-     
-      
-      const text = {
-        email,
-        password,
-      }
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const onPressSignin = useCallback(() => {
-      dispatch(loginAction(text))
-      
-      
-      props.navigation.goBack()
-    }, [email, password]);
+  const errMessage = useSelector((state) => state.login?.data);
 
-    return(
+  const text = {
+    email,
+    password,
+  };
 
-      <Container>
-        <Header props={props}/>
-        <Contents>
+  const onPressSignin = useCallback(() => {
+    dispatch(loginAction(text));
+    props.navigation.goBack();
+  }, [email, password]);
 
-      
-      <Button title="회원가입하기" onPress={() => props.navigation.navigate('SignUp')} />
-      <Button title="Go back" onPress={() => props.navigation.goBack()} />
+  return (
+    <Container>
+      <Header props={props} />
+      <Contents>
+        <Button
+          title="회원가입하기"
+          onPress={() => props.navigation.navigate('SignUp')}
+        />
+        <Button title="Go back" onPress={() => props.navigation.goBack()} />
 
-     {/* <TouchableOpacity style = {{ margin: 100 }} onPress = {goToHome}>
+        {/* <TouchableOpacity style = {{ margin: 100 }} onPress = {goToHome}>
          <Text>Log In</Text>
       </TouchableOpacity> */}
-      <Text>
-       Login
-      </Text>
-      <TextInput
-      placeholder = "Email"
-      type="email"
-      value={email}
-      onChangeText={ email => setEmail(email)} >
+        <Text>Login</Text>
+        <TextInput
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChangeText={(email) => setEmail(email)}></TextInput>
 
-      </TextInput>
+        <TextInput
+          placeholder="Password"
+          type="password"
+          value={password}
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}></TextInput>
 
-      <TextInput
-      placeholder = "Password"
-      type = "password"
-      value={password}
-      secureTextEntry={true} 
-      onChangeText={ password => setPassword(password)}>
-      </TextInput>
-      {/* {isLogin ? (
-            <Button
-              title = "log out"
-              onClick={handleIsLogin}
-            >
-
-            </Button>
-          ) : (
-            <Button
-            title = "log in"
-            handleIsLogin={handleIsLogin} >
-                </Button>
-          )} */}
-      <InButton
-        onPress={onPressSignin}
-        title='Log in'
-        // disabled={!isLogin}
-        // title={isLogin ? "Log in" : "Log out"}
-      />
+        <InButton
+          onPress={onPressSignin}
+          title="Log in"
+          // disabled={!isLogin}
+          // title={isLogin ? "Log in" : "Log out"}
+        />
       </Contents>
-      <Nav props={props}/>
+      <Nav props={props} />
     </Container>
-    )
-}
+  );
+};
 
-
-
-
-  export default SignIn;
+export default SignIn;
