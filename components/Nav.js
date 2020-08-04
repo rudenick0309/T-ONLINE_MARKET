47de3c1fa410991fd4768dc29bb0644cb579ea8d
rudenick0309 <rudenick0309@gmail.com>
2020-08-05 00:23:32 +0900

    "feat css layout, added fontAwesome, verctorIcon"

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 import {StyleSheet, Text, View, Input, TouchableOpacity} from "react-native";
 import Home from "../pages/Home";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import {faChevronLeft, faSearch,faHome} from "@fortawesome/free-solid-svg-icons";
+import {faAddressCard} from "@fortawesome/free-regular-svg-icons";
 
 const NavView = styled.View`
   height: 50px;
@@ -11,13 +14,13 @@ const NavView = styled.View`
   align-items: center;
   border-radius: 50px;
   background-color: #f5efef;
-`
+`;
 const NavIcon = styled.TouchableOpacity`
   justify-content:center;
   align-items:center;
   width : 50px;
   height : 50px;
-`
+`;
 
 const NavText = styled.Text`
   font-weight : bold;
@@ -30,16 +33,49 @@ const NavCenterText = styled.Text`
   width: 70px;
   font-size: 20px;
   color: #464e46;
-`
+`;
+
+const HeaderIcon = styled.TouchableOpacity`
+  justify-content:center;
+  align-items:center;
+  width : 55px;
+  width : 50px;
+  height : 50px;
+`;
+
+const HeaderIconText = styled.Text`
+  color: #464e46;
+  font-weight : bold;
+`;
 
 const Nav = ({props}) => {
   return (
     <NavView>
-      <NavIcon onPress={ () => {props.navigation.goBack()}} ><NavText>BACK</NavText></NavIcon>
-      <NavIcon onPress={ () => {props.navigation.navigate('LOGIN')} } ><NavCenterText>SignIn</NavCenterText></NavIcon>
-      <NavIcon onPress={ () => {props.navigation.navigate('Mypage')} }><NavText>MY PAGE</NavText></NavIcon>
+      <NavIcon onPress={() => {
+        props.navigation.goBack();
+      }}>
+        <FontAwesomeIcon icon={ faChevronLeft } color={ '#464e46' } size={ 25 }/>
+      </NavIcon>
+
+      <NavIcon onPress={() => {
+        props.navigation.navigate("LOGIN");
+      }}>
+        <NavCenterText>
+          <HeaderIcon onPress={() => {
+            props.navigation.navigate("Search");
+          }}>
+            <FontAwesomeIcon icon={ faSearch } color={ '#464e46' } size={ 25 }/>
+          </HeaderIcon>
+        </NavCenterText>
+      </NavIcon>
+
+      <NavIcon onPress={() => {
+        props.navigation.navigate("Mypage");
+      }}>
+        <FontAwesomeIcon icon={ faAddressCard } color={ '#464e46' } size={ 30 }/>
+      </NavIcon>
     </NavView>
-  )
-}
+  );
+};
 
 export default Nav;
