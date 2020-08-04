b6ff1fc1be1d5d6e06bd45fc87f8a734a3a98ff8
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-08-04 12:01:09 +0900

    FIX signip saga

--- sagas/signupsaga.js
+++ sagas/signupsaga.js
@@ -1,49 +1,30 @@
-import axios from "axios";
-import {POST_SIGNUP_REQUEST,
-    POST_SIGNUP_SUCCESS,
-    POST_SIGNUP_FAILURE
-} from "../reducers/signup";
-import {all, fork, call, put, takeLatest, throttle} from "redux-saga/effects";
+import axios from 'axios';
+import {
+  POST_SIGNUP_REQUEST,
+  POST_SIGNUP_SUCCESS,
+  POST_SIGNUP_FAILURE,
+} from '../reducers/signup';
+import {all, fork, call, put, takeLatest, throttle} from 'redux-saga/effects';
 
 console.log('In saga, at 0 : ', 'saga executes');
 // 4
 
-
 function PostSignupAPI(data) {
   console.log('In goods of SAGA 4, : ', data);
   // return axios.post("/goods/info/qa_lists", data)
-  return axios.post("http://ec2-15-164-219-204.ap-northeast-2.compute.amazonaws.com:4000/user/signup", data)
+  return axios.post('/user/signup', data);
 }
 
-
-
 // 3
-// function* loadQnA(action) {
-//   try {
-//     yield delay(1000);
-//     // const result = yield call(loadQnAAPI, action.data);
-//     yield put({
-//       type: LOAD_QUESTION_SUCCESS,
-//       // TODO : data: result.data,
-//       id: action.data,
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     yield put({
-//       type: LOAD_QUESTION_FAILURE,
-//       error: err.response.data,
-//     });
-//   }
-// }
 
 function* PostSignup(action) {
   console.log('In goods of SAGA, at 3 : ', action);
 
   try {
-    // TODO: const result = yield call(addQnAAPI, action.data);  no 'data', It's 'text'.
+    const result = yield call(PostSignupAPI, action.data);
     yield put({
       type: POST_SIGNUP_SUCCESS,
-      data: action.text,
+      data: result.text,
     });
   } catch (err) {
     console.log(err);
@@ -54,9 +35,6 @@ function* PostSignup(action) {
   }
 }
 
-
-
-
 // 2
 
 function* watchPostSignup() {
@@ -65,12 +43,10 @@ function* watchPostSignup() {
   console.log('In goods of SAGA 2 - 1, : ');
 }
 
-
 // 1
 export default function* signupSaga() {
   yield all([
     fork(watchPostSignup),
     // fork(watchLogout),
-   
   ]);
 }
