e78fe92dbfd4e98a7775c7c9f70972d253673c37
rudenick0309 <rudenick0309@gmail.com>
2020-08-04 15:29:45 +0900

    "simply css-layout"

--- components/RecommendedFlower.js
+++ components/RecommendedFlower.js
@@ -5,21 +5,24 @@ import styled from "styled-components";
 // css part
 export const RecommendedFlowerContainer = styled.TouchableOpacity`
   flex: 1;
-  
   flex-direction:row;
 `;
 
 export const RecommendedFlowerContents = styled.View`
   flex: 1;
-  
+  background-color:white;
+  border-radius: 30px;
 `;
 
 export const RecommendedFlowerImageView = styled.Image`
   flex:1;
   resize-mode: contain;
-  
 `;
 
 export const RecommendedFlowerTextView = styled.Text`
-  color : red;
+  color : #222831;
+  padding-left : 20px;
+  font-size: 15px;
+  margin-top:10px;
+  text-align:center;
 `;
