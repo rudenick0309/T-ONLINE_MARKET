1b024a4af1c053471e18b13e39f2622417f6042d
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-08-04 11:58:09 +0900

    FIX reducer resign

--- reducers/resign.js
+++ reducers/resign.js
@@ -1,15 +1,12 @@
-
-
 export const initialState = {
-    resignLoding: false,
-    resignError: null,
-    resignDone: false,
-    // log:[],
-    // logOutLoding: false,
-    // logOutError: null,
-    // logOutDone: false,
-  };
- 
+  resignLoding: false,
+  resignError: null,
+  resignDone: false,
+  // log:[],
+  // logOutLoding: false,
+  // logOutError: null,
+  // logOutDone: false,
+};
 
 export const POST_RESIGN_REQUEST = 'POST_RESIGN_REQUEST';
 export const POST_RESIGN_SUCCESS = 'POST_RESIGN_SUCCESS';
@@ -19,14 +16,12 @@ export const POST_RESIGN_FAILURE = 'POST_RESIGN_FAILURE';
 // export const POST_LOGOUT_SUCCESS = 'POST_LOGOUT_SUCCESS';
 // export const POST_LOGOUT_FAILURE = 'POST_LOGOUT_FAILURE';
 
-
-export const resignAction = (data)  => {
-  console.log("resignAction :", data)
+export const resignAction = (data) => {
+  console.log('resignAction :', data);
   return {
     type: POST_RESIGN_REQUEST,
     data,
-  }
-  
+  };
 };
 
 // export const logoutAction = data => {
@@ -34,43 +29,37 @@ export const resignAction = (data)  => {
 //        type: POST_LOGOUT_REQUEST };
 // };
 
-
-  
-
 const reducer = (state = initialState, action) => {
-    switch(action.type){
-        case POST_RESIGN_REQUEST:
-          console.log("reducer resign request", action)
+  switch (action.type) {
+    case POST_RESIGN_REQUEST:
+      console.log('reducer resign request', action);
       return {
         ...state,
         resignLoding: true,
         resignError: null,
         resignDone: false,
-      }
-      case POST_RESIGN_SUCCESS: 
-      console.log("reducer resign success",action);
+      };
+    case POST_RESIGN_SUCCESS:
+      console.log('reducer resign success', action);
       return {
         ...state,
         resignLoding: false,
         data: action.payload,
         resignDone: true,
       };
-    
+
     case POST_RESIGN_FAILURE:
-      console.log("reducer resign failed", action)
+      console.log('reducer resign failed', action);
       return {
         ...state,
         resignLoding: false,
         resignError: true,
         resignDone: false,
       };
-  
 
-      default:
-     return state;
-    }
-}
+    default:
+      return state;
+  }
+};
 
- 
-
- export default reducer;
+export default reducer;
