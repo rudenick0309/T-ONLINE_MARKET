42d39981f51347cf4b5da3966c9e390bc9680c56
rudenick0309 <rudenick0309@gmail.com>
2020-08-05 13:42:57 +0900

    "simply modified css"

--- components/BucketList.js
+++ components/BucketList.js
@@ -2,46 +2,46 @@ import React from 'react';
 import {View,Text,Image} from "react-native";
 import styled from "styled-components";
 
-
-
 const ViewStyled = styled.View`
   flex:1;
   height: 100px;
   flex-direction:row;
   justify-content : space-between;
-  border: 3px solid green;
 `
 
 const ImageOfUpperLeft = styled.Image`
   resize-mode: contain;
   flex:1;
   width:100px;
-  border: 2px solid red;
 `;
 
+const TextViewStyled = styled.View`
+  margin-bottom: 20px;
+`
+
+
 const BucketList = (props) => {
+  console.log('In BucketList, all the props : ', img);
   const img = props.data.goods_img;
   const name = props.data.goods_name;
   const price = props.data.goods_price;
-  const count = props.data.count;
 
-  console.log('In BucketList, all the props : ', img);
 
   return (
     <ViewStyled>
 
-      <View>
+      <TextViewStyled>
         <ImageOfUpperLeft source={{uri: img}} />
-      </View>
+      </TextViewStyled>
 
-      <View>
+      <TextViewStyled>
         <Text>{name}</Text>
         <Text>{price}</Text>
-      </View>
+      </TextViewStyled>
 
-      <View>
-        <Text>{count}</Text>
-      </View>
+      {/*<View>*/}
+      {/*  <Text>{count}</Text>*/}
+      {/*</View>*/}
 
     </ViewStyled>
   )
