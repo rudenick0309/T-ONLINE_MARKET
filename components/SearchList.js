4d27f26400fe50d65c1982329b96cb3e61b4a00c
rudenick0309 <rudenick0309@gmail.com>
2020-08-05 13:45:32 +0900

    "feat divided object for one api routes and modified css layout"

--- components/SearchList.js
+++ components/SearchList.js
@@ -1,14 +1,75 @@
-import React from 'react';
-import {View, Text} from 'react-native'
+import React from "react";
+import {View, Text, Image,TouchableOpacity} from "react-native";
 import styled from "styled-components";
 
+const ViewStyled = styled.View`
+  flex:1;
+  height: 100px;
+  flex-direction:row;
+  justify-content : space-between;
+  margin-bottom : 20px;
+  border-bottom-width:1px;
+  border-style: solid;
+  border-color: grey;
+  border-radius: 20px;
+`;
+
+const ImageOfUpperLeft = styled.Image`
+  resize-mode: contain;
+  flex:1;
+  width:100px;
+`;
+
+const ImageViewStyled = styled.View`
+  margin-left: 30px;
+  margin-bottom: 13px;
+`;
+
+const TextViewStyled = styled.View`
+  margin-top: 20px;
+  padding-right: 30px;
+  align-items: flex-end;
+`;
+
+const TextStyled = styled.Text`
+  font-size: 15px;
+  margin-bottom:3px;
+`;
+
+
+
 const SearchList = (props) => {
-  console.log('In SearchList, props : ', props);
+  console.log("In SearchList, props : ", props);
+  const img = props.data.goods_img;
+  const name = props.data.goods_name;
+  const price = props.data.goods_price;
+  const id = props.data.goods_id;
+
+  // const onPressToBuckt =(num)=> {
+  //   props.navigation.navigate('GoodsDetail', {id: id})
+  // }
+
   return (
-    <View>
-      <Text>SearchList rendered</Text>
-    </View>
-  )
-}
+    <ViewStyled >
+
+      {/*<TouchableOpacity onPress={onPressToBuckt}>*/}
+
+        <ImageViewStyled>
+          <ImageOfUpperLeft source={{uri: img}}/>
+        </ImageViewStyled>
+
+        <TextViewStyled>
+          <TextStyled>{name}</TextStyled>
+          <TextStyled>{price}</TextStyled>
+        </TextViewStyled>
+
+        {/*<View>*/}
+        {/*  /!*<Text>{count}</Text>*!/*/}
+        {/*</View>*/}
+      {/*</TouchableOpacity>*/}
+
+    </ViewStyled>
+  );
+};
 
 export default SearchList;
