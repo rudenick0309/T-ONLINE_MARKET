7d57a5d4ba375de1261c0f8d89f0bac9353d7142
rudenick0309 <rudenick0309@gmail.com>
2020-08-06 01:45:17 +0900

    "add a page user writes a review"

---
+++ pages/ReviewPlusEdit.js
@@ -0,0 +1,89 @@
+import React, {useCallback, useState} from "react";
+import {View, Text, TextInput, Button} from "react-native";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import styled from "styled-components";
+import {useDispatch, useSelector} from "react-redux";
+import {patchToReview} from "../reducers/goods";
+
+// css part
+const Container = styled.ScrollView`
+  flex: 1;
+  border: 3px solid blue
+`;
+
+const InContainer = styled.TouchableOpacity`
+  border: 3px solid ivory
+`;
+
+const Contents = styled.View`
+  flex: 1;
+  border: 3px solid grey;
+`;
+
+const ImageView = styled.Image`
+  flex:1
+  border: 2px solid yellow;
+`;
+
+const TextView = styled.Text`
+  color : red;
+`;
+
+const TextInputStyled = styled.TextInput`
+  border: 3px solid red;
+`;
+
+//
+const ReviewPlusEdit = (props) => {
+  const {id, title, contents, username} = props.route.params;
+  console.log("In ReviewPlusEdit props  : ", props);   //TODO : must check console.log for study
+  const dispatch = useDispatch();
+  const {patchReviewDone} = useSelector(state => state.goods)
+  console.log('In ReviewPlusEdit patchReviewDone : ', patchReviewDone);
+
+  const [editTitle, onChangeTextTitle] = useState(title);
+  const [editContent, onChangeTextContent] = useState(contents);
+  const [nonEditUserName, setNonEditUserName] = useState(username);
+
+
+  const onChangeTextUserName = (text) => {
+    // setEditUserName(text);
+  };
+
+  const modifiedQnA = {
+    username,
+    title: editTitle,
+    content: editContent,
+    id,
+  }
+
+  const onPressEditReview = () => {
+    console.log('In ReviewPluseEdit modifiedReview : ', id);
+    dispatch(patchToReview(modifiedReview))
+    onChangeTextTitle(title)
+    onChangeTextContent(contents)
+    props.navigation.goBack();
+  }
+
+  return (
+    <Container>
+      <Header props={props}/>
+
+      <Contents>
+
+        <View>
+          <TextInputStyled placeholder={nonEditUserName} value={nonEditUserName} editable={false} />
+          <TextInputStyled placeholder={editTitle} value={editTitle} onChangeText={(text) => onChangeTextTitle(text)} />
+          <TextInputStyled placeholder={editContent} value={editContent} onChangeText={(text) => onChangeTextContent(text)} />
+        </View>
+        <Button title={"수정"} onPress={onPressEditReview} />
+
+      </Contents>
+
+      <Nav props={props}/>
+    </Container>
+  );
+};
+
+export default ReviewPlusEdit;
