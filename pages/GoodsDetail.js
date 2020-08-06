9c4e331820207a9b853eaebb67dcf6e30d250c2d
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-08-06 10:48:01 +0900

    SIMPLY goodsDetail

--- pages/GoodsDetail.js
+++ pages/GoodsDetail.js
@@ -1,36 +1,36 @@
-import React, {useState, useEffect, useCallback} from "react";
-import {StyleSheet, Text, View, Button} from "react-native";
-import styled from "styled-components";
-import Header from "../components/Header";
-import Nav from "../components/Nav";
+import React, {useState, useEffect, useCallback} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import styled from 'styled-components';
+import Header from '../components/Header';
+import Nav from '../components/Nav';
 // import RecommendedFlower from "../components/RecommendedFlower";
-import Payment from "./Payment";
-import Bucket from "./Bucket";
-import {useSelector, useDispatch} from "react-redux";
-import QnAList from "../components/QnAList";
-import {countDefault, countPlus, countMinus, loadGoodsInfo} from "../reducers/goods";
-import goods from "../sagas/goods";
-import QnADetailInfo from "../components/QnADetailInfo";
-import ReviewDetailInfo from "../components/ReviewDetailInfo";
-import AsyncStorage from "@react-native-community/async-storage";
-
-
-
+import Payment from './Payment';
+import Bucket from './Bucket';
+import {useSelector, useDispatch} from 'react-redux';
+import QnAList from '../components/QnAList';
+import {
+  countDefault,
+  countPlus,
+  countMinus,
+  loadGoodsInfo,
+} from '../reducers/goods';
+import goods from '../sagas/goods';
+import QnADetailInfo from '../components/QnADetailInfo';
+import ReviewDetailInfo from '../components/ReviewDetailInfo';
+import AsyncStorage from '@react-native-community/async-storage';
 
 // css part
 const Container = styled.SafeAreaView`
   flex: 1;
-  
 `;
 
 const Contents = styled.ScrollView`
   flex: 1;
-  
 `;
 
 const DetailInfoOfUpper = styled.View`
   flex: 1;
-  
+
   flex-direction: row;
 `;
 
@@ -39,12 +39,11 @@ const LeftDetailInfoOfUpper = styled.View`
   flex: 1;
   width: 200px;
   height: 200px;
-  
 `;
 
 const ImageOfUpperLeft = styled.Image`
   flex: 1;
-  width:100%;
+  width: 100%;
   height: 100%;
   resize-mode: contain;
 `;
@@ -54,33 +53,27 @@ const RightDetailInfoOfUpper = styled.View`
   flex: 1;
   width: 200px;
   height: 200px;
- 
 `;
 
 const TextOfUpperRight = styled.Text`
   flex: 1;
- 
 `;
-
 
 // bottom components
 const DetailInfoOfBottom = styled.View`
   flex: 1;
-  
 `;
 
 // info detail part
 const InfoDetailInfoOfBottom = styled.ScrollView`
   flex: 1;
   height: 500px;
-  
 `;
 
 const ImageInfoOfBottom = styled.Image`
   flex: 1;
   height: 500px;
   resize-mode: contain;
-  
 `;
 
 // qna detail part
@@ -91,13 +84,13 @@ const QnADetailInfoOfBottom = styled.ScrollView`
 `;
 
 const QnAheader = styled.View`
-  flex-direction : row;
-  justify-content : space-around;
+  flex-direction: row;
+  justify-content: space-around;
 `;
 
 const QnAButtonDetailInfoOfBottom = styled.Button`
   height: 150px;
-  width : 400px;
+  width: 400px;
   border: 20px solid black;
 `;
 
@@ -116,17 +109,15 @@ const QnAButtonDetailInfoOfBottom = styled.Button`
 const ViewDetailInfoOfBottom = styled.View`
   flex: 1;
   flex-direction: row;
-  justify-content:space-around;
+  justify-content: space-around;
 `;
 
 const ViewRowStyled = styled.View`
-  flex-direction : row;
+  flex-direction: row;
 `;
-
 
 // // TODO: position : fixed? -> always stay in right and bottom corner in a mobile view?
 const ButtonDetailInfoOfBottom = styled.Button`
-
   border: 3px solid red;
 `;
 
@@ -136,7 +127,7 @@ const GoodsDetail = (props) => {
   const [userQnA, setQnA] = useState(false);
   const [review, setReview] = useState(false);
   const dispatch = useDispatch();
-  const qna = useSelector(state => state.goods?.qna);
+  const qna = useSelector((state) => state.goods?.qna);
   const id = props.route.params;
   const goodsInfo = useSelector((state) => state.goods?.goodsInfo);
   // console.log("In GOODS_DETAIL, props : ", props);
@@ -147,18 +138,14 @@ const GoodsDetail = (props) => {
   const count = useSelector((state) => state.goods?.count);
 
   const data = {
-    count : count,
-    id : id,
-    goods_name : goods_name,
-    goods_img :goods_img,
-    goods_price : goods_price,
-  }
+    count: count,
+    id: id,
+    goods_name: goods_name,
+    goods_img: goods_img,
+    goods_price: goods_price,
+  };
 
-
-
-
-
-  console.log("In GOODSDETAIL, count : ", count);
+  console.log('In GOODSDETAIL, count : ', count);
 
   useEffect(() => {
     // TODO: In here, qna states are re-rendering? Or, In render part, qna states are re-rendering? TEST!
@@ -168,46 +155,45 @@ const GoodsDetail = (props) => {
   }, []);
 
   const onPressPlus = useCallback(() => {
-    dispatch(countPlus())
-  }, [])
+    dispatch(countPlus());
+  }, []);
 
   const onPressMinus = useCallback(() => {
-    dispatch(countMinus())
-  }, [])
+    dispatch(countMinus());
+  }, []);
 
   return (
     <Container>
-      <Header props={props}/>
+      <Header props={props} />
 
       <Contents>
         <DetailInfoOfUpper>
-
           <LeftDetailInfoOfUpper>
-            <ImageOfUpperLeft source={{uri: goods_img}}/>
+            <ImageOfUpperLeft source={{uri: goods_img}} />
           </LeftDetailInfoOfUpper>
 
           <RightDetailInfoOfUpper>
             <TextOfUpperRight>{goods_name}</TextOfUpperRight>
             <TextOfUpperRight>{goods_price}</TextOfUpperRight>
-            <TextOfUpperRight>{"꽃말"}</TextOfUpperRight>
+            <TextOfUpperRight>{'꽃말'}</TextOfUpperRight>
 
             <ViewRowStyled>
-              <Button title={"수량 감소"} onPress={onPressMinus}/>
+              <Button title={'수량 감소'} onPress={onPressMinus} />
               <Text>{count}</Text>
-              <Button title={"수량 증가"} onPress={onPressPlus}/>
+              <Button title={'수량 증가'} onPress={onPressPlus} />
             </ViewRowStyled>
 
             <ViewRowStyled>
               <Button
-                title={"구매하기"}
+                title={'구매하기'}
                 onPress={() => {
-                  return props.navigation.navigate("Payment");
+                  return props.navigation.navigate('Payment');
                 }}
               />
               <Button
-                title={"장바구니 담기"}
+                title={'장바구니 담기'}
                 onPress={() => {
-                  props.navigation.navigate("Bucket", data);
+                  props.navigation.navigate('Bucket', data);
                 }}
               />
             </ViewRowStyled>
@@ -218,8 +204,8 @@ const GoodsDetail = (props) => {
         <DetailInfoOfBottom>
           <ViewDetailInfoOfBottom>
             <ButtonDetailInfoOfBottom
-              title={"info"}
-              color={"palevioletred"}
+              title={'info'}
+              color={'palevioletred'}
               onPress={() => {
                 setQnA(false);
                 setInfo(true);
@@ -227,8 +213,8 @@ const GoodsDetail = (props) => {
               }}
             />
             <ButtonDetailInfoOfBottom
-              title={"userQnA"}
-              color={"palevioletred"}
+              title={'userQnA'}
+              color={'palevioletred'}
               onPress={() => {
                 setQnA(true);
                 setInfo(false);
@@ -236,8 +222,8 @@ const GoodsDetail = (props) => {
               }}
             />
             <ButtonDetailInfoOfBottom
-              title={"review"}
-              color={"palevioletred"}
+              title={'review'}
+              color={'palevioletred'}
               onPress={() => {
                 setQnA(false);
                 setInfo(false);
@@ -246,34 +232,21 @@ const GoodsDetail = (props) => {
             />
           </ViewDetailInfoOfBottom>
 
-          {info
-            ?
-            (
-              <InfoDetailInfoOfBottom>
-                <ImageInfoOfBottom source={{uri: info_img}}/>
-              </InfoDetailInfoOfBottom>
-            )
-            : userQnA
-              ?
-              (
-                <QnADetailInfo prop={props}/>
-              )
-              : review
-                ?
-
-                (
-                  <ReviewDetailInfo prop={props}/>
-
-                )
-                :
-                (
-                  <Text>empty part</Text>
-                )}
+          {info ? (
+            <InfoDetailInfoOfBottom>
+              <ImageInfoOfBottom source={{uri: info_img}} />
+            </InfoDetailInfoOfBottom>
+          ) : userQnA ? (
+            <QnADetailInfo prop={props} />
+          ) : review ? (
+            <ReviewDetailInfo prop={props} />
+          ) : (
+            <Text>empty part</Text>
+          )}
         </DetailInfoOfBottom>
-
       </Contents>
 
-      <Nav props={props}/>
+      <Nav props={props} />
     </Container>
   );
 };
