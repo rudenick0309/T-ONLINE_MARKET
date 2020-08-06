df6ac6b9e809fd1a9239268642bffd0e7f1ff80c
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-08-06 10:46:04 +0900

    FIX nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import {StyleSheet, Text, View, Input, TouchableOpacity} from 'react-native';
 import Home from '../pages/Home';
+import {useSelector} from 'react-redux';
 
 const NavView = styled.View`
   height: 50px;
@@ -15,13 +16,41 @@ const NavIcon = styled.TouchableOpacity`
 `;
 
 const Nav = ({props}) => {
+  const loginDone = useSelector((state) => state.login?.loginDone);
+  const userInfo = useSelector((state) => state.login.data?.userInfo);
+  const message = useSelector((state) => state.login.data?.message);
+  // console.log('usertype', userInfo.user_type);
+
   return (
     <NavView>
+      <NavIcon
+        onPress={() => {
+          props.navigation.navigate('Home');
+        }}>
+        <Text>Home Icon</Text>
+      </NavIcon>
+      {loginDone && message ? (
+        <NavIcon
+          onPress={() => {
+            props.navigation.navigate('MypageSeller');
+          }}>
+          <Text>My page Seller Icon</Text>
+        </NavIcon>
+      ) : (
+        <NavIcon
+          onPress={() => {
+            props.navigation.navigate('Mypage');
+          }}>
+          <Text>Msy page Icon</Text>
+        </NavIcon>
+      )}
 
-      <NavIcon onPress={ () => {props.navigation.navigate('Home')} } ><Text>Home Icon</Text></NavIcon>
-      <NavIcon onPress={ () => {props.navigation.navigate('Mypage')} }><Text>My page Icon</Text></NavIcon>
-      <NavIcon onPress={ () => {props.navigation.goBack()}} ><Text>back</Text></NavIcon>
-
+      <NavIcon
+        onPress={() => {
+          props.navigation.goBack();
+        }}>
+        <Text>back</Text>
+      </NavIcon>
     </NavView>
   );
 };
