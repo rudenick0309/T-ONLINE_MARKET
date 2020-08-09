eebb42198e915b77d4097f3ab3608e0f16dc7376
rudenick0309 <rudenick0309@gmail.com>
2020-08-09 13:58:24 +0900

    "feat css layout - editing review article for a item at review board"

--- pages/ReviewPlusEdit.js
+++ pages/ReviewPlusEdit.js
@@ -7,31 +7,50 @@ import {useDispatch, useSelector} from "react-redux";
 import {patchToReview, timesToDelete} from "../reducers/goods";
 
 // css part
-const Container = styled.ScrollView`
+const Container = styled.SafeAreaView`
   flex: 1;
-  border: 3px solid blue
-`;
-
-const InContainer = styled.TouchableOpacity`
-  border: 3px solid ivory
+  padding : 10px;
 `;
 
 const Contents = styled.View`
   flex: 1;
-  border: 3px solid grey;
-`;
-
-const ImageView = styled.Image`
-  flex:1
-  border: 2px solid yellow;
-`;
-
-const TextView = styled.Text`
-  color : red;
+  
 `;
 
 const TextInputStyled = styled.TextInput`
-  border: 3px solid red;
+   margin-bottom: 10px;
+  flex: 1;
+  border: 2px solid grey  
+  height: 50px;
+  border-radius:10px;
+`;
+
+const TopTextView = styled.View`
+  flex:0.7;
+  opacity:0.2
+  justify-content:center;
+  align-items:center;
+  margin: 20px 0px;
+`;
+
+const EditButton = styled.TouchableOpacity`
+  flex:0.2;
+  background-color:#535204;
+  justify-content:center;
+  align-items:center;
+  border-radius:10px;  
+`;
+
+const EditButtonText = styled.Text`
+  color:white;
+  font-size:30px;
+  letter-spacing:10px;
+`;
+
+const MiddleView = styled.View`
+  flex:0.8
+  margin : 30px 0px
+  top: -50px
 `;
 
 //
@@ -39,7 +58,7 @@ const ReviewPlusEdit = (props) => {
   const {id, title, contents, username} = props.route.params;
   // console.log("In ReviewPlusEdit props  : ", props);   //TODO : must check console.log for study
   const dispatch = useDispatch();
-  const {patchReviewDone} = useSelector(state => state.goods)
+  const {patchReviewDone} = useSelector(state => state.goods);
   // console.log('In ReviewPlusEdit patchReviewDone : ', patchReviewDone);
 
   const [editTitle, onChangeTextTitle] = useState(title);
@@ -56,22 +75,22 @@ const ReviewPlusEdit = (props) => {
     modifiedReview = {
       title: editTitle,
       contents: editContent,
-      review_id : id,
-    }
-  }, [editTitle,editContent])
+      review_id: id,
+    };
+  }, [editTitle, editContent]);
 
 
   const onPressEditReview = () => {
     // console.log('In ReviewPluseEdit modifiedReview : ', id);
-    dispatch(patchToReview(modifiedReview))
+    dispatch(patchToReview(modifiedReview));
 
-    onChangeTextTitle(title)
-    onChangeTextContent(contents)
+    onChangeTextTitle(title);
+    onChangeTextContent(contents);
     setTimeout(() => {
       props.navigation.goBack();
-    }, 2000)
-    dispatch(timesToDelete())
-  }
+    }, 2000);
+    dispatch(timesToDelete());
+  };
 
   return (
     <Container>
@@ -79,12 +98,39 @@ const ReviewPlusEdit = (props) => {
 
       <Contents>
 
-        <View>
-          <TextInputStyled placeholder={nonEditUserName} value={nonEditUserName} editable={false} />
-          <TextInputStyled placeholder={editTitle} value={editTitle} onChangeText={(text) => onChangeTextTitle(text)} />
-          <TextInputStyled placeholder={editContent} value={editContent} onChangeText={(text) => onChangeTextContent(text)} />
-        </View>
-        <Button title={"수정"} onPress={onPressEditReview} />
+        <TopTextView>
+          <Text>
+            {
+              `저희 제품을 구매해 주셔서 감사드립니다`
+            }
+          </Text>
+          <Text>
+            {
+              `고객님의 소중한 후기가 저희에겐 큰 힘이 됩니다`
+            }
+          </Text>
+        </TopTextView>
+
+        <MiddleView>
+          <TextInputStyled
+            placeholder={nonEditUserName}
+            value={nonEditUserName}
+            editable={false}
+          />
+
+          <TextInputStyled
+            placeholder={editContent}
+            value={editContent}
+            maxLength={200}
+            multiline={true}
+            onChangeText={(text) => onChangeTextContent(text)}
+            editable={true}
+          />
+        </MiddleView>
+
+        <EditButton onPress={onPressEditReview}>
+          <EditButtonText>수정하기</EditButtonText>
+        </EditButton>
 
       </Contents>
 
